ef2eb0f1b25cc42a1396ef078adba657a59b51ac
akitsu-sanae <akitsu.sanae@gmail.com>
2016-12-12 22:42:22 +0900

    add license notation

--- wrapper.cpp
+++ wrapper.cpp
@@ -1,3 +1,10 @@
+/*============================================================================
+  Copyright (C) 2016 akitsu sanae
+  https://github.com/akitsu-sanae/asd-c-wrapper
+  Distributed under the Boost Software License, Version 1.0. (See accompanying
+  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+============================================================================*/
+
 #include "Altseed.h"
 
 extern "C" {
